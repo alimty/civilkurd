8fd5a9c6b48afd4b6638df4988374ba71ef2344a
ali Mengütay <alozicyber@gmail.com>
2020-12-01 23:59:14 +0100

    so on so forth

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Civil Kurd website.</p>
-    <p>This will be a platform</p>
+    <p>This will be a platform and so on so forth</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
